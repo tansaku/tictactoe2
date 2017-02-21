f19c06185d2df6850a1575c45be6908e7161dad0
Sam Joseph <tansaku@gmail.com>
2017-02-21 13:27:53 +0000

    game entity now handle moves by asking player about move and hthen updating board

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,15 @@
+require_relative 'tictactoe'
+
 class Player
-  def choose_move(array)
-    array.sample
+  def initialize(rules_klass = TicTacToe)
+    @rules_klass = rules_klass
   end
+
+  def choose_move(board)
+    Array(rules_klass.valid_moves(board)).sample
+  end
+
+  private 
+
+  attr_reader :rules_klass
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,10 +2,13 @@ require 'player'
 
 describe Player do
 
-  subject(:player) { described_class.new }
+  subject(:player) { described_class.new(rules_klass) }
+  let(:board) { double :board}
+  let(:rules_klass) { double :rules_klass}
 
   it 'can choose a move' do
-    expect([:A1, :B2, :C3]).to include player.choose_move([:A1, :B2, :C3])
+    allow(rules_klass).to receive(:valid_moves).with(board).and_return(:A1)
+    expect(player.choose_move(board)).to eq :A1
   end
   
 end
